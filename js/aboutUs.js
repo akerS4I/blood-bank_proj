933dd858f9eb499e35d0acaf5142853ac4ba7839
akerS4I <108138366+akerS4I@users.noreply.github.com>
2022-10-23 11:51:42 +0500

    Add files via upload

---
+++ js/aboutUs.js
@@ -0,0 +1,35 @@
+// const step = 1;
+// const out1 = document.querySelector("#out-1");
+// const out2 = document.querySelector("#out-2");
+
+// function outNum(num, elem, time) {
+//   n = 0;
+//   let t = Math.round(time / (num / step));
+//   let interval = setInterval(() => {
+//     n = n + step;
+//     if (n == num) {
+//       clearInterval(interval);
+//     }
+//     elem.innerHTML = n;
+//   }, t);
+// }
+
+// function outNum2(num2, elem2, time2) {
+//   n = 0;
+//   let t = Math.round(time2 / (num2 / step));
+//   let interval = setInterval(() => {
+//     n = n + step;
+//     if (n == num2) {
+//       clearInterval(interval);
+//     }
+//     elem2.innerHTML = n;
+//   }, t);
+// }
+// outNum(1000, out1, 1000);
+// outNum2(100, out2, 2000);
+
+
+
+
+
+
